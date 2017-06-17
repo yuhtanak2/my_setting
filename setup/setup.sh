d8892dd5858f5192acb8093147ec3adb50bd7591
yuhtanak2 <eternal.innocence.g@gmail.com>
2017-06-17 18:42:05 +0900

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -38,6 +38,7 @@ if [ ! -e ~/.anyenv ]; then
   git clone https://github.com/riywo/anyenv  ~/.anyenv
   mkdir -p $(anyenv root)/plugins
   git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+  git clone https://github.com/znz/anyenv-git.git $(anyenv root)/plugins/anyenv-git
 fi
 
 shopt -s dotglob
